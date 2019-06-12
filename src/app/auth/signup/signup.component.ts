70b6feaa910cb60e9e7c7a7d68f76dab95cf7e26
knvishal <vik@narola.email>
2019-06-12 10:29:51 +0530

    updated project

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { from } from 'rxjs';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  isLoading: false;
+  constructor(public authService: AuthService) { }
+
+  onSignup(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    this.authService.createUser(form.value.email, form.value.password);
+  }
+
+}
